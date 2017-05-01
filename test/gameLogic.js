48c2e689535a906bc3429b0e74d863fefdd9b3ba
Zack Winchell <zackwinchell@gmail.com>
2017-05-01 14:57:18 -0700

    Add basic, incomplete, tests

---
+++ test/gameLogic.js
@@ -0,0 +1,19 @@
+const { expect } = require('chai');
+
+describe('Game Logic', function() {
+  it('should start with X\'s turn', function() {
+    expect(false).to.be.true;
+  });
+
+  it('should alternate turns', function() {
+    expect(false).to.be.true;
+  });
+
+  it('should detect when a player has won', function() {
+    expect(false).to.be.true;
+  });
+
+  it('should detect when the game is a draw', function() {
+    expect(false).to.be.true;
+  });
+});
---
+++ test/rendering.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+
+describe('Render', function() {
+  it('should render a blank game when neither player has made a move', function() {
+    expect(false).to.be.true;
+  });
+
+  it('should render whose turn it is', function() {
+    expect(false).to.be.true;
+  });
+
+  it('should render when a move is made', function() {
+    expect(false).to.be.true;
+  });
+});
